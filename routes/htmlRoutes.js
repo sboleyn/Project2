bd8f981618445cf2c2eee740bd98a45d22e0e452
Sami B <sboleyn@email.arizona.edu>
2018-11-03 20:49:05 -0700

    changes to sam

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,38 +2,40 @@ var db = require("../models");
 var Op = db.Sequelize.Op;
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    return res.render("./public/index.html");
-  });
-
-  app.get("/profile/:id", function(req,res) {
-    db.User.findByID(req.params.id).then(function(userData) {
-      res.render("profile", {
-        userData: userData
-      });
+    // Load index page
+    app.get("/", function(req, res) {
+        return res.render("profile");
     });
-  });
 
-  app.get("/search", function(req, res) {
-    return res.render("./public/search.html");
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/search/:name", function(req, res) {
-    db.Friends.findAll({ where: {
-      name: { 
-        [Op.like]: req.params.name 
-      }
-    } }).then(function(UsersData) {
-      res.render("searchResults", {
-        example: UsersData
-      });
+    app.get("/profile/:id", function(req, res) {
+        db.User.findByID(req.params.id).then(function(userData) {
+            res.render("profile", {
+                userData: userData
+            });
+        });
     });
-  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+    app.get("/search", function(req, res) {
+        return res.render("./public/search.html");
+    });
+
+    // Load example page and pass in an example by id
+    app.get("/search/:name", function(req, res) {
+        db.Friends.findAll({
+            where: {
+                name: {
+                    [Op.like]: req.params.name
+                }
+            }
+        }).then(function(UsersData) {
+            res.render("searchResults", {
+                example: UsersData
+            });
+        });
+    });
+
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+        res.render("404");
+    });
 };
