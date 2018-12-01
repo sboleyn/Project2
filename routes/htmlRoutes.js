d7e8a500db2f3c8117e4069f671b23efa73ca9bd
Sami B <sboleyn@email.arizona.edu>
2018-11-30 19:47:01 -0700

    changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,50 +1,20 @@
 var db = require("../models");
-var friendsData = require("../models/friends");
+var userData = require("../data/users");
 var Op = db.Sequelize.Op;
 
 module.exports = function(app) {
     // Load index page
     app.get("/", function(req, res) {
         return res.sendFile("index.html");
-    });  
-
-    app.post("/profile/:name", function(req, res) {
-        db.User.findOne({
-            where: {
-                name: req.params.name 
-            }
-        }).then(function(userData) {
-            console.log(userData)
-            return res.render("profile",userData);
-        }); 
     });
 
-    app.get("/userprofile", function(req,res) {
-        db.User.findOne({
-            where: {
-                name: "Derek"
-            }
-        }).then(function(userData) {
-            db.Friend.findAll({
-                where: {
-                    id: [1,2,3]
-                }
-            }).then(function (dbFriends) {
-                db.Rating.findAll({
-                    where: {
-                        id: [1,2,3]
-                    }
-                }).then(function (ratings) {
-                    data = {
-                        user: userData,
-                        friends: dbFriends,
-                        ratings: ratings
-                    };
-                    return res.render("profile", data)
-                })
-              });
-        });
-    })
+    app.get("/profile/:id", function(req, res) {
+        // db.User.findByID(req.params.id).then(function(userData) {
+        // res.json(userData);
+        return res.render("profile", userData[req.params.id - 1]);
+
+    });
+    // });
 
     app.get("/search", function(req, res) {
         return res.render("search");
@@ -58,9 +28,9 @@ module.exports = function(app) {
                     [Op.like]: req.params.name
                 }
             }
-        }).then(function(UsersDat) {
+        }).then(function(UsersData) {
             res.render("searchResults", {
-                example: friendsData
+                example: UsersData
             });
         });
     });
